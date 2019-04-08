fbcf43638cdd5dafe17010ceee582ba5a347c0a2
Parinon Suriyo <parinon12@gmail.com>
2019-04-09 02:31:51 +0700

    Refactoring "trip_duration_stats"
    By using .format

--- bikeshare.py
+++ bikeshare.py
@@ -121,10 +121,10 @@ def trip_duration_stats(df):
 
     # TO DO: display total travel time
     total_travel_time = df['Trip Duration'].sum()
-    print('Total travel time is: ', total_travel_time)
+    print('Total travel time is: {}'.format(total_travel_time))
     # TO DO: display mean travel time
     mean_travel_time = df['Trip Duration'].mean()
-    print('Mean travel time is: ', mean_travel_time)
+    print('Mean travel time is: {}'.format(mean_travel_time))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
